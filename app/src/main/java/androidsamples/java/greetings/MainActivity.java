73b8417ec802390ad2e4b20aad0987ed555017e7
Dev Goel <goeldev01@gmail.com>
2021-09-17 00:00:08 +0530

    Added color to action bar

--- app/src/main/java/androidsamples/java/greetings/MainActivity.java
+++ app/src/main/java/androidsamples/java/greetings/MainActivity.java
@@ -1,7 +1,9 @@
 package androidsamples.java.greetings;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar bar = getSupportActionBar();
+        assert bar != null;
+        bar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.teal_200)));
     }
 }
